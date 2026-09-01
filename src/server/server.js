f437254416ec30e4e6664b61633b6c2c22c18070
John Baird <john.e.baird5@gmail.com>
2026-08-31 19:24:04 -0600

    serving html

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const publicDir = path.join(__dirname, "..") //Targeting the /src
+
+
+
 app.get("/", (req, res) => {
-	res.send("Server is running");
+	res.sendFile(path.join(publicDir, "index.html"));
 });
+
+
 
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
