8a269f01a496785e031eb5fe6d43a8a8dc6f6226
Parker Hemming <parker@hemm.ing>
2026-08-31 20:33:20 -0600

    homepage styling

--- src/app/pages/convert/convert.page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-convert-page',
-  standalone: true,
-  templateUrl: './convert.page.html',
-  styleUrl: './convert.page.scss',
-})
-export class ConvertPage {}
---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ConvertComponent } from '../../components/convert/convert.component';
+import { MatDivider } from '@angular/material/divider';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [ConvertComponent, MatDivider],
+  standalone: true,
+  templateUrl: './home.page.html',
+  styleUrl: './home.page.scss',
+})
+export class HomePage {}
